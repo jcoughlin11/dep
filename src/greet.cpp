8ffdeebc7be1d8e0fa8e565fee4e23bf4320148c
Jared <jcoughlin@dephy.com>
2022-09-20 12:31:40 -0400

    Adds inc subdir.

--- src/greet.cpp
+++ src/greet.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 
+#include "goodbye.hpp"
 #include "greet.hpp"
 
 
 void greet(void)
 {
     std::cout << "Hello world!" << std::endl;
+}
+
+void goodbye(void)
+{
+    std::cout << "Goodbye!" << std::endl;
 }
